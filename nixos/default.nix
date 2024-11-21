11f367f43ca96c33129a089e0d93be3d736a8d7b
Jordan Williams <jordan@jwillikers.com>
2024-11-21 14:17:07 -0600

    Fix sudo config
    There isn't support for SETENV in sudo-rs yet.

--- nixos/default.nix
+++ nixos/default.nix
@@ -106,54 +106,55 @@
       #   %wheel \${config.hostname} = NOPASSWD: /usr/bin/poweroff , /usr/bin/systemctl poweroff , /usr/bin/reboot , /usr/bin/systemctl reboot , /usr/bin/systemctl suspend
       #   \${username} \${config.hostname} = NOPASSWD: /usr/bin/sudo -H -u core fish -c 'cd; fish'
       # '';
-      # extraRules = [
-      #   {
-      #   commands = [
-      #     {
-      #       command = "${pkgs.systemd}/bin/systemctl poweroff";
-      #       options = [ "NOPASSWD" ];
-      #     }
-      #     {
-      #       command = "${pkgs.systemd}/bin/systemctl reboot";
-      #       options = [ "NOPASSWD" ];
-      #     }
-      #     {
-      #       command = "${pkgs.systemd}/bin/systemctl suspend";
-      #       options = [ "NOPASSWD" ];
-      #     }
-      #     {
-      #       command = "${pkgs.systemd}/bin/poweroff";
-      #       options = [ "NOPASSWD" ];
-      #     }
-      #     {
-      #       command = "${pkgs.systemd}/bin/reboot";
-      #       options = [ "NOPASSWD" ];
-      #     }
-      #   ];
-      #   # host = "ALL";
-      #   # host = config.networking.fqdn;
-      #   users = [ username ];
-      # }
-      # {
-      #   commands = [
-      #     # todo Test if this works.
-      #     # Need EXEC option?
-      #     # sudo -H -u core fish -c 'cd; fish'
-      #     {
-      #       command = "${pkgs.lib.getExe pkgs.fish} -c 'cd; fish'";
-      #       options = [ "NOPASSWD" "SETENV" ];
-      #     }
-      #     {
-      #       command = "${pkgs.lib.getExe pkgs.fish}";
-      #       options = [ "NOPASSWD" "SETENV" ];
-      #     }
-      #   ];
-      #   # host = config.networking.fqdn;
-      #   # host = "ALL";
-      #   # runAs = "core:core";
-      #   runAs = "core";
-      #   users = [username];
-      # }];
+      extraRules = [
+        {
+          commands = [
+            {
+              command = "${pkgs.systemd}/bin/systemctl poweroff";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "${pkgs.systemd}/bin/systemctl reboot";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "${pkgs.systemd}/bin/systemctl suspend";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "${pkgs.systemd}/bin/poweroff";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "${pkgs.systemd}/bin/reboot";
+              options = [ "NOPASSWD" ];
+            }
+          ];
+          # host = "ALL";
+          # host = config.networking.fqdn;
+          users = [ username ];
+          # }
+          # {
+          #   commands = [
+          #     # todo Test if this works.
+          #     # Need EXEC option?
+          #     # sudo -H -u core fish -c 'cd; fish'
+          #     {
+          #       command = "${pkgs.lib.getExe pkgs.fish} -c 'cd; fish'";
+          #       options = [ "NOPASSWD" "SETENV" ];
+          #     }
+          #     {
+          #       command = "${pkgs.lib.getExe pkgs.fish}";
+          #       options = [ "NOPASSWD" "SETENV" ];
+          #     }
+          #   ];
+          #   # host = config.networking.fqdn;
+          #   # host = "ALL";
+          #   # runAs = "core:core";
+          #   runAs = "core";
+          #   users = [username];
+        }
+      ];
     };
   };
 
