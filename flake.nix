960ddaf2cc2ba1831952c09db7ef933965d73bd8
Jordan Williams <jordan@jwillikers.com>
2024-11-22 18:30:32 -0600

    Fix error for check

--- flake.nix
+++ flake.nix
@@ -169,13 +169,13 @@
         formatter = treefmtEval.config.build.wrapper;
         packages = {
           default = self.packages.${system}.piceiver-sd-image;
-          pkgs = pkgsArmCross;
           piceiver-sd-image = self.nixosConfigurations.piceiver.config.system.build.sdImage;
           piceiver-sd-image-native = self.nixosConfigurations.piceiver-native.config.system.build.sdImage;
           snappellite-sd-image = self.nixosConfigurations.snappellite.config.system.build.sdImage;
           snappellite-sd-image-native =
             self.nixosConfigurations.snappellite-native.config.system.build.sdImage;
         };
+        # pkgs = pkgsArmCross;
       }
     )
     // {
