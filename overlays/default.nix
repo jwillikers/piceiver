c8c3e61ed36b214b6181803bab82f1202d925ee8
Jordan Williams <jordan@jwillikers.com>
2024-11-23 21:01:12 -0600

    Fix inconsistent GSteamer version with gst-python

--- overlays/default.nix
+++ overlays/default.nix
@@ -61,40 +61,61 @@
     # todo Make it possible to disable graphviz support in libcamera.
     graphviz = prev.graphviz.override { withXorg = false; };
     gst_all_1 = prev.gst_all_1 // {
-      gstreamer = prev.gst_all_1.gstreamer.overrideAttrs (prevAttrs: rec {
-        version = "1.24.9";
-        src = prev.fetchurl {
-          url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
-          hash = "sha256-6/R7a+71CKAMhVfUwfFxPlx++bpw2sRd7tgOGCvPJg8=";
-        };
-      });
-      gst-plugins-base =
-        (prev.gst_all_1.gst-plugins-base.override {
-          enableX11 = false;
-          enableWayland = false;
-        }).overrideAttrs
-          (prevAttrs: rec {
-            version = "1.24.9";
-            src = prev.fetchurl {
-              url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
-              hash = "sha256-W7O5RpB9POBN2EK2EMgRHCsGETUbJaH6Iq9e+ol4V8s=";
-            };
-          });
-      gst-plugins-good =
-        (prev.gst_all_1.gst-plugins-good.override {
-          enableX11 = false;
-          enableWayland = false;
-        }).overrideAttrs
-          (prevAttrs: rec {
-            version = "1.24.9";
-            src = prev.fetchurl {
-              url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
-              hash = "sha256-iX3lC/8zfjyi+G8eqijggo2DAkFWFipQxOoK+G4peZ8=";
-            };
-          });
+      # gstreamer = prev.gst_all_1.gstreamer.overrideAttrs (prevAttrs: rec {
+      #   version = "1.24.9";
+      #   src = prev.fetchurl {
+      #     url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
+      #     hash = "sha256-6/R7a+71CKAMhVfUwfFxPlx++bpw2sRd7tgOGCvPJg8=";
+      #   };
+      # });
+      # gst-plugins-bad = prev.gst_all_1.gst-plugins-bad.overrideAttrs (prevAttrs: rec {
+      #   version = "1.24.9";
+      #   src = prev.fetchurl {
+      #     url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
+      #     hash = "sha256-Nvz3qa8KdTtDuwO5g1JG901y9xJDaeZqHi3HsE9aXKs=";
+      #   };
+      # });
+      gst-plugins-base = prev.gst_all_1.gst-plugins-base.override {
+        enableX11 = false;
+        enableWayland = false;
+      };
+      # }).overrideAttrs
+      #   (prevAttrs: rec {
+      #     # version = "1.24.9";
+      #     # src = prev.fetchurl {
+      #     #   url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
+      #     #   hash = "sha256-W7O5RpB9POBN2EK2EMgRHCsGETUbJaH6Iq9e+ol4V8s=";
+      #     # };
+      #   });
+      gst-plugins-good = prev.gst_all_1.gst-plugins-good.override {
+        enableX11 = false;
+        enableWayland = false;
+      };
+      # }).overrideAttrs
+      #   (prevAttrs: rec {
+      #     version = "1.24.9";
+      #     src = prev.fetchurl {
+      #       url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
+      #       hash = "sha256-iX3lC/8zfjyi+G8eqijggo2DAkFWFipQxOoK+G4peZ8=";
+      #     };
+      #   });
+      # gst-libva = prev.gst_all_1.gst-libva.overrideAttrs (prevAttrs: rec {
+      #   version = "1.24.9";
+      #   src = prev.fetchurl {
+      #     url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
+      #     hash = "sha256-MmgumuUI7gH0+xNLOlIAgeKsAHIgmXV3YksdFhcdRWw=";
+      #   };
+      # });
       gst-plugins-rs = prev.gst_all_1.gst-plugins-rs.override {
         withGtkPlugins = false;
       };
+      # gst-plugins-ugly = prev.gst_all_1.gst-plugins-ugly.overrideAttrs (prevAttrs: rec {
+      #   version = "1.24.9";
+      #   src = prev.fetchurl {
+      #     url = "https://gstreamer.freedesktop.org/src/${prevAttrs.pname}/${prevAttrs.pname}-${version}.tar.xz";
+      #     hash = "sha256-S2swEQ84zQXrZ0IilxQrdaVf4AADEF9IsTYD5nYcw7Y=";
+      #   };
+      # });
     };
     gtk3 = prev.gtk3.override {
       broadwaySupport = false;
@@ -174,7 +195,7 @@
           prev.pipewire
         ];
 
-      dependencies = with prev.python3Packages; [
+      dependencies = with final.python3Packages; [
         gst-python
         pygobject3
         pykka
